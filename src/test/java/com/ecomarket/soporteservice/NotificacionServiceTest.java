ee4d529df86635236a88e2838efad4d52fb967a4
Hans <hwnss.w@gmail.com>
2026-06-06 01:32:42 -0400

    Se soluciono problema al ejecutar pruebas de service, era debido a que como CanalNotificacion se usa como una entidad por parametro, necesitamos datos de la misma entonces se debe pasar su repositorio como otro Mock mas.
    Se agrego prueba unitaria de sendNotificacion y de readAllNotificaciones.

--- src/test/java/com/ecomarket/soporteservice/NotificacionServiceTest.java
+++ src/test/java/com/ecomarket/soporteservice/NotificacionServiceTest.java
@@ -1,41 +1,39 @@
 package com.ecomarket.soporteservice;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.web.client.RestTemplate;
 
+import com.ecomarket.soporteservice.dto.ClienteDTO;
 import com.ecomarket.soporteservice.model.entity.Notificacion;
 import com.ecomarket.soporteservice.model.reference.CanalNotificacion;
 import com.ecomarket.soporteservice.repository.NotificacionRepository;
+import com.ecomarket.soporteservice.service.CanalNotificacionService;
 import com.ecomarket.soporteservice.service.NotificacionService;
 
 public class NotificacionServiceTest {
-//EJEMPLO GUIA 
-/*
-
-Mascota mascota = new Mascota(null, "Rex", "Perro", 5);
-        Mascota mascotaGuardada = new Mascota(1L, "Rex", "Perro", 5);
-
-        when(mascotaRepository.save(mascota)).thenReturn(mascotaGuardada);
-
-        Mascota resultado = mascotaService.guardarMascota(mascota);
-
-        assertThat(resultado).isEqualTo(mascotaGuardada);
-        assertThat(resultado.getId()).isEqualTo(1L);
-        assertThat(resultado.getNombre()).isEqualTo("Rex");
-
-*/
 
     @Mock
     private NotificacionRepository notificacionRepository;
+
+    @Mock
+    private CanalNotificacionService canalNotificacionService;
+
+    @Mock
+    private RestTemplate restTemplate;
 
     @InjectMocks
     private NotificacionService notificacionService;
@@ -46,32 +44,56 @@ Mascota mascota = new Mascota(null, "Rex", "Perro", 5);
     }
 
     @Test
+    void testReadAllNotificaciones() {
+        Long canalId = 1L;
+        String titulo = "Test send notificacion svc";
+        String cuerpo = "dadadadadadadadadadaddadadaada";
+
+        CanalNotificacion canalMock = canalNotificacionService.getCanalNotificacionById(canalId);
+
+        Notificacion notificacion = Notificacion.builder()
+        .id(1L).destinatarioId(1L).canal(canalMock).titulo(titulo).cuerpo(cuerpo)
+        .fechaEnvioNotificacion(LocalDateTime.now()).enviadaConExito(true).build();
+
+        List<Notificacion> notificaciones = new ArrayList<>();
+        notificaciones.add(notificacion);
+
+        when(notificacionRepository.findAll()).thenReturn(notificaciones);
+
+        List<Notificacion> resultado = notificacionService.readAllNotificacion();
+
+        assertThat(resultado).hasSize(1);
+        assertThat(resultado).contains(notificacion);
+
+        verify(notificacionRepository).findAll();
+    }
+
+    @Test
     void testSendNotificacion() {
-        CanalNotificacion canal = new CanalNotificacion(1L, "SMS");
-        
-        //!!!!!!!!! IMPORTANTE !!!!!!!!!!!!!
-        // PENDIENTE MODIFICACION DE ALGUNOS SERVICES, INCLUIR DTO PARA QUE SE PUEDAN REALIZAR CIERTAS PRUEBAS
-        // EL LA VALIDACION DE ERROR DE CANALNOTIFICACIONSERVICE PROVOCA UN ERROR NO DESEADO EN testNotificacionService
 
-        @SuppressWarnings("static-access")
-        Notificacion notificacion = new Notificacion().builder()
-        .id(null).destinatarioId(1L).canal(canal).titulo("Test send notificacion svc").cuerpo("dadadada")
+        Long canalId = 1L;
+        Long destinatarioId = 1L;
+        String titulo = "Test send notificacion svc";
+        String cuerpo = "dadadadadadadadadadaddadadaada";
+
+        CanalNotificacion canalMock = canalNotificacionService.getCanalNotificacionById(canalId);
+
+        Notificacion notificacionGuardadaDB = Notificacion.builder()
+        .id(1L).destinatarioId(1L).canal(canalMock).titulo(titulo).cuerpo(cuerpo)
         .fechaEnvioNotificacion(LocalDateTime.now()).enviadaConExito(true).build();
 
-        @SuppressWarnings("static-access")
-        Notificacion notificacionEnviada = new Notificacion().builder()
-        .id(1L).destinatarioId(1L).canal(canal).titulo("Test send notificacion svc").cuerpo("dadadada")
-        .fechaEnvioNotificacion(LocalDateTime.now()).enviadaConExito(true).build();
+        when(canalNotificacionService.getCanalNotificacionById(1L)).thenReturn(canalMock);
+        when(restTemplate.getForObject(any(String.class), eq(ClienteDTO.class))).thenReturn(new ClienteDTO());
+        when (notificacionRepository.save(any(Notificacion.class))).thenReturn(notificacionGuardadaDB);
 
-        when(notificacionRepository.save(notificacion)).thenReturn(notificacionEnviada);
+        Notificacion resultado = notificacionService.sendNotificacion(destinatarioId, titulo, cuerpo, canalId);
+
+        assertThat(resultado).isNotNull();
+        assertThat(resultado.getId()).isEqualTo(1L);
+        assertThat(resultado.getTitulo()).isEqualTo(titulo);
+        assertThat(resultado.getEnviadaConExito()).isTrue();
         
-        Notificacion resultado = notificacionService.sendNotificacion(1L, "Test send notificacion svc", "dadadada", 1L);
-
-        assertThat(resultado).isEqualTo(notificacionEnviada);
-        assertThat(resultado.getId()).isEqualTo(notificacionEnviada.getId());
-        assertThat(resultado.getEnviadaConExito()).isEqualTo(true);
-
-        verify(notificacionRepository.save(notificacion));
+        verify(notificacionRepository).save(any(Notificacion.class));
     }
 
 }
